42044d904d8d54de72f29edb39896cf4d69de88a
Stampers <56046444+Stampers@users.noreply.github.com>
2019-10-30 09:36:38 -0700

    Modified playerController.cs and followPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(0, 6, -12);
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-public float speed = 5.0f;
+private float speed = 20.0f;
+private float turnSpeed = 45.0f;
+private float horizontalInput;
+private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,13 @@ public float speed = 5.0f;
     // Update is called once per frame
     void Update()
     {
-        // Move Vehicle Forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // moves the car forward base on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
