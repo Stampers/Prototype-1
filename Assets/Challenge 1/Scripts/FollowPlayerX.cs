7f0a885e6ab8ba6b35856e1b12405de7efc0d7a3
Stampers <56046444+Stampers@users.noreply.github.com>
2019-11-12 11:46:08 -0800

    Imported Challenge

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject Plane;
+    private Vector3 offset = new Vector3(3, 3, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Plane.transform.position + offset;
+    }
+}
